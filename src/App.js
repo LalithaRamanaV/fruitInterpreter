2ef89f9c4c76fe928daf0116538e4d07583ae4bb
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-09-01 13:41:07 +0530

    build: updated app.js

--- src/App.js
+++ src/App.js
@@ -33,28 +33,58 @@ export default function App() {
     setMeaning(meaning);
   }
   return (
-    <div className="App">
-      <h1>Fruit Interpreter</h1>
-      <p>Click on the below emoji to know the name</p>
-      <input
-        onChange={emojiEntered}
-        placeholder="Enter fruit emoji or press th emoji"
-      ></input>
-      <h2>{meaning}</h2>
+    <>
+      <div className="App">
+        <h1>Fruit Interpreter</h1>
+        <p>Click on the below emoji to know the name</p>
+        <input
+          onChange={emojiEntered}
+          placeholder="Enter fruit emoji or press th emoji"
+        ></input>
+        <h2>{meaning}</h2>
 
-      <div>
-        {emojiArray.map((item) => {
-          return (
-            <span
-              onClick={() => emojiClicked(item)}
-              className="emoji"
-              key={item}
-            >
-              {item}
-            </span>
-          );
-        })}
+        <div>
+          {emojiArray.map((item) => {
+            return (
+              <span
+                onClick={() => emojiClicked(item)}
+                className="emoji"
+                key={item}
+              >
+                {item}
+              </span>
+            );
+          })}
+        </div>
       </div>
-    </div>
+
+      <footer className="footer">
+        <div className="footer-header">Connect with me here</div>
+        <ul className="list-non-bullet">
+          <li className="list-item-inline">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+              href="https://github.com/LalithaRamanaV"
+            >
+              <i className="fab fa-github" aria-hidden="true"></i>
+            </a>
+          </li>
+
+          <li className="list-item-inline">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="link"
+              href="https://www.linkedin.com/in/lalithavadavalli/"
+            >
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </li>
+        </ul>
+        <p>Copyright Lalitha Vadavalli</p>
+      </footer>
+    </>
   );
 }
